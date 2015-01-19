6f5d7a0191b59c51b7da2b9f72ce612af2c08586
dibus2 <florian@15-10875.local>
2015-01-18 19:34:10 -0600

    fix a bug introduced in version 1.0.5!!When non squared Yuk are used.

--- Source/GroupTheory/RGEsmathModule.py
+++ Source/GroupTheory/RGEsmathModule.py
@@ -442,7 +442,7 @@ def determinordering(model,Final):
 											Indices[-1] = Indices[-1][0],Symbol(str(Indices[-1][1]).split('_')[0]+'1_f')
 										Term = model.Classes[str(Term[0])](*Indices[-1]).transpose()
 									else :
-										if Indices[-1][0] == Indices[-1][1] and len(str(Indices[-1][1]).split('_')) == 2 :
+										if len(Indices[-1]) >= 2 and Indices[-1][0] == Indices[-1][1] and len(str(Indices[-1][1]).split('_')) == 2 :
 											Indices[-1] = Indices[-1][0],Symbol(str(Indices[-1][1]).split('_')[0]+'1_f')
 										Term = model.Classes[str(Term[0])](*Indices[-1])
 								else :
